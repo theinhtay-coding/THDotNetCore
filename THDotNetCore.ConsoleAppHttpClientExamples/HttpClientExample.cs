b0f2df41066276a48b790a53c23d482b0254d3d9
TheinHtay-67 <theinhtay.dev@gmail.com>
2024-05-11 22:47:05 +0630

    Add HttpClientExample

---
+++ THDotNetCore.ConsoleAppHttpClientExamples/HttpClientExample.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace THDotNetCore.ConsoleAppHttpClientExamples
+{
+    public class HttpClientExample
+    {
+        private readonly HttpClient _client = new HttpClient() { BaseAddress = new Uri("https://localhost:7132") };
+        private readonly string _blogEndpoint = "api/Blog";
+
+        public async Task RunAsync()
+        {
+            //await ReadAsync();
+            //await EditAsync(1);
+            //await EditAsync(100);
+            //await CreateAsync("title", "author", "content");
+            await UpdateAsync(18, "title update", "author update", "content update");
+            //await PatchAsync(17, "title patch");
+        }
+
+        private async Task ReadAsync()
+        {
+            var response = await _client.GetAsync(_blogEndpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                List<BlogDto> lst = JsonConvert.DeserializeObject<List<BlogDto>>(jsonStr)!;
+
+                foreach (BlogDto item in lst)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(item));
+                    Console.WriteLine($"Title => {item.BlogTitle}");
+                    Console.WriteLine($"Author => {item.BlogAuthor}");
+                    Console.WriteLine($"Content => {item.BlogContent}");
+                }
+            }
+        }
+
+        private async Task EditAsync(int id)
+        {
+            var response = await _client.GetAsync($"{_blogEndpoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                var item = JsonConvert.DeserializeObject<BlogDto>(jsonStr)!;
+
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+                Console.WriteLine($"Title => {item.BlogTitle}");
+                Console.WriteLine($"Author => {item.BlogAuthor}");
+                Console.WriteLine($"Content => {item.BlogContent}");
+            }
+            else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+
+        private async Task CreateAsync(string title, string author, string content)
+        {
+            BlogDto blogDto = new BlogDto()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+            //To Json
+            var blogJson = JsonConvert.SerializeObject(blogDto);
+
+            HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+            var response = await _client.PostAsync(_blogEndpoint, httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+
+        private async Task UpdateAsync(int id, string title, string author, string content)
+        {
+            BlogDto blogDto = new BlogDto()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+            //To Json
+            var blogJson = JsonConvert.SerializeObject(blogDto);
+
+            HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+            var response = await _client.PatchAsync($"{_blogEndpoint}/{id}", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+
+        private async Task patchasync(int id, string title, string author, string content)
+        {
+            //BlogDto blogdto = new BlogDto()
+            //{
+            //    BlogTitle = title,
+            //    BlogAuthor = author,
+            //    BlogContent = content
+            //};
+
+            ////to json
+            //var blogjson = JsonConvert.SerializeObject(blogdto);
+
+            //HttpContent httpcontent = new StringContent(blogjson, Encoding.UTF8, Application.Json);
+            //var response = await _client.PutAsync($"{_blogEndpoint}/{id}", httpcontent);
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    string message = await response.Content.ReadAsStringAsync();
+            //    Console.WriteLine(message);
+            //}
+        }
+
+        private async Task DeleteAsync(int id)
+        {
+            var response = await _client.DeleteAsync($"{_blogEndpoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+            else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ THDotNetCore.ConsoleAppHttpClientExamples/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using THDotNetCore.ConsoleAppHttpClientExamples;
+
+Console.WriteLine("Hello, World!");
+
+//Console App - Client (Frontend)
+//Asp.Net Core Web API - Servar (Backend)
+
+//HttpClient client = new HttpClient();
+//HttpResponseMessage response = await client.GetAsync("https://localhost:7132/api/Blog");
+//if (response.IsSuccessStatusCode)
+//{
+//    string jsonStr = await response.Content.ReadAsStringAsync();
+//    //Console.WriteLine(jsonStr);
+//    List<BlogDto> lst = JsonConvert.DeserializeObject<List<BlogDto>>(jsonStr)!;
+
+//    foreach(BlogDto model in lst)
+//    {
+//        Console.WriteLine(JsonConvert.SerializeObject(model));
+//        //Console.WriteLine($"Id => {model.BlogId}");
+//        //Console.WriteLine($"Title => {model.BlogTitle}");
+//        //Console.WriteLine($"Author => {model.BlogAuthor}");
+//        //Console.WriteLine($"Content => {model.BlogContent}");
+//    }
+//}
+
+HttpClientExample example = new HttpClientExample();
+await example.RunAsync();
+
+Console.ReadLine();
