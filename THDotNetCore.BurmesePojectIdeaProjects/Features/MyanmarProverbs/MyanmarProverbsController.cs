f3c0358faa28e60f0ca20636e061d9048d801412
TheinHtay-67 <theinhtay.dev@gmail.com>
2024-05-12 10:37:30 +0630

    Add MyanmarProverbs Project

---
+++ THDotNetCore.BurmesePojectIdeaProjects/Features/MyanmarProverbs/MyanmarProverbsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using THDotNetCore.BurmesePojectIdeaProjects.Models;
+
+namespace THDotNetCore.BurmesePojectIdeaProjects.Features.MyanmarProverbs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyanmarProverbsController : ControllerBase
+    {
+        private readonly MMProverbModel _data;
+        public MyanmarProverbsController()
+        {
+
+        }
+
+        private async Task<MMProverbModel> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("MyanmarProverbs_Data.json");
+            var model = JsonConvert.DeserializeObject<MMProverbModel>(jsonStr);
+            return model!;
+        }
+
+        [HttpGet("title")]
+        public async Task<IActionResult> Titles()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_MMProverbsTitle);
+        }
+
+        [HttpGet("subtitle/{titleId}")]
+        public async Task<IActionResult> GetSubtitleById(int titleId)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_MMProverbs
+                            .Where(x => x.TitleId == titleId)
+                            .Select(x => new { x.ProverbId, x.ProverbName })
+                            .ToList());
+        }
+
+        [HttpGet("mmproverbs/{titleId}/{proverbId}")]
+        public async Task<IActionResult> Proverbs(int titleId, int proverbId)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_MMProverbs.Where(x => x.TitleId == titleId && x.ProverbId == proverbId).FirstOrDefault());
+        }
+    }
+}
