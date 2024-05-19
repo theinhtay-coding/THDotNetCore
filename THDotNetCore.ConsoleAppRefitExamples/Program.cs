e8b966a2f09f7482abf624893f70e4236f333bf8
TheinHtay-67 <theinhtay.dev@gmail.com>
2024-05-20 00:30:51 +0630

    [Add] RefitClientExamples

---
+++ THDotNetCore.ConsoleAppRefitExamples/Program.cs
@@ -0,0 +1,11 @@
+﻿using THDotNetCore.ConsoleAppRefitExamples;
+
+try
+{
+RefitExample refitExample = new RefitExample();
+await refitExample.RunAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message.ToString());
+}
---
+++ THDotNetCore.ConsoleAppRefitExamples/RefitExample.cs
@@ -0,0 +1,90 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THDotNetCore.ConsoleAppRefitExamples
+{
+    public class RefitExample
+    {
+        private readonly IBlogApi _service = RestService.For<IBlogApi>("https://localhost:7132");
+        public async Task RunAsync()
+        {
+            await ReadAsync();
+            //await EditAsync(1);
+            //await EditAsync(100);
+            //await CreateAsync("title", "author", "content");
+            //await UpdateAsync(18, "title update", "author update", "content update");
+            //await PatchAsync(17, "title patch");
+        }
+
+        public async Task ReadAsync()
+        {
+            var lst = await _service.GetBlogs();
+            foreach (var item in lst)
+            {
+                Console.WriteLine($"Id => {item.BlogId}");
+                Console.WriteLine($"Title => {item.BlogTitle}");
+                Console.WriteLine($"Author => {item.BlogAuthor}");
+                Console.WriteLine($"Content => {item.BlogContent}");
+                Console.WriteLine("------------------------------");
+            }
+        }
+
+        public async Task EditAsync(int id)
+        {
+            try
+            {
+                var item = await _service.GetBlog(id);
+                Console.WriteLine($"Id => {item.BlogId}");
+                Console.WriteLine($"Title => {item.BlogTitle}");
+                Console.WriteLine($"Author => {item.BlogAuthor}");
+                Console.WriteLine($"Content => {item.BlogContent}");
+                Console.WriteLine("------------------------------");
+            }
+            catch (ApiException ex)
+            {
+                Console.WriteLine(ex.StatusCode.ToString());
+                Console.WriteLine(ex.Content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+        private async Task CreateAsync(string title, string author, string content)
+        {
+            BlogModel blog = new BlogModel()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            var message = await _service.CreateBlog(blog);
+            Console.WriteLine(message);
+        }
+
+        private async Task UpdateAsync(int id,string title, string author, string content)
+        {
+            BlogModel blog = new BlogModel()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            var message = await _service.UpdateBlog(id,blog);
+            Console.WriteLine(message);
+        }
+
+        private async Task DeleteAsync(int id)
+        {
+            var message = await _service.DeleteBlog(id);
+            Console.WriteLine(message);
+        }
+    }
+}
