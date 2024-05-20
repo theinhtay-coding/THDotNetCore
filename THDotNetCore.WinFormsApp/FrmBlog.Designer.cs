108309e76aa5ca62dc8a8be090f99eb417e14a24
TheinHtay-67 <theinhtay.dev@gmail.com>
2024-05-20 20:21:35 +0630

    [Add] WiindowFormApp

---
+++ THDotNetCore.WinFormsApp/FrmBlog.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace THDotNetCore.WinFormsApp
+{
+    partial class FrmBlog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTitle = new TextBox();
+            txtAuthor = new TextBox();
+            txtContent = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnSave = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(283, 90);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(440, 30);
+            txtTitle.TabIndex = 0;
+            // 
+            // txtAuthor
+            // 
+            txtAuthor.Location = new Point(283, 168);
+            txtAuthor.Name = "txtAuthor";
+            txtAuthor.Size = new Size(440, 30);
+            txtAuthor.TabIndex = 1;
+            // 
+            // txtContent
+            // 
+            txtContent.Location = new Point(283, 246);
+            txtContent.Multiline = true;
+            txtContent.Name = "txtContent";
+            txtContent.Size = new Size(440, 109);
+            txtContent.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(283, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 23);
+            label1.TabIndex = 3;
+            label1.Text = "Title :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(283, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 23);
+            label2.TabIndex = 4;
+            label2.Text = "Author :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(283, 220);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 23);
+            label3.TabIndex = 5;
+            label3.Text = "Content :";
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.FromArgb(67, 160, 71);
+            btnSave.FlatAppearance.BorderSize = 0;
+            btnSave.FlatStyle = FlatStyle.Flat;
+            btnSave.Location = new Point(405, 377);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(99, 40);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "&Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = Color.FromArgb(229, 57, 53);
+            btnCancel.FlatAppearance.BorderSize = 0;
+            btnCancel.FlatStyle = FlatStyle.Flat;
+            btnCancel.Location = new Point(283, 377);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(99, 40);
+            btnCancel.TabIndex = 7;
+            btnCancel.Text = "&Cancel";
+            btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // FrmBlog
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1053, 518);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtContent);
+            Controls.Add(txtAuthor);
+            Controls.Add(txtTitle);
+            Font = new Font("Segoe UI", 10F);
+            ForeColor = Color.White;
+            Name = "FrmBlog";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Blog";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtTitle;
+        private TextBox txtAuthor;
+        private TextBox txtContent;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnSave;
+        private Button btnCancel;
+    }
+}
---
+++ THDotNetCore.WinFormsApp/FrmBlog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using THDotNetCore.Shared;
+using THDotNetCore.WinFormsApp.Models;
+using THDotNetCore.WinFormsApp.Queries;
+
+namespace THDotNetCore.WinFormsApp
+{
+    public partial class FrmBlog : Form
+    {
+        private readonly DapperService _dapperService;
+        public FrmBlog()
+        {
+            InitializeComponent();
+            _dapperService = new DapperService(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearControl();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BlogModel blog = new BlogModel();
+                blog.BlogTitle = txtTitle.Text.Trim();
+                blog.BlogAuthor = txtAuthor.Text.Trim();
+                blog.BlogContent = txtContent.Text.Trim();
+
+                int result = _dapperService.Execute(BlogQuery.BlogCreate, blog);
+                string message = result > 0 ? "Saving Successful." : "Saving Failed";
+
+                var messageBoxIcon = result > 0 ? MessageBoxIcon.Information : MessageBoxIcon.Error;
+                MessageBox.Show(message, "Blog" , MessageBoxButtons.OK, messageBoxIcon);
+                ClearControl();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+        public void ClearControl()
+        {
+            txtTitle.Clear();
+            txtAuthor.Clear();
+            txtContent.Clear();
+            txtTitle.Focus();
+        }
+    }
+}
