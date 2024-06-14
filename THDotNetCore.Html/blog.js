49669c6238ff303d953f80cb6e59624e76a9d600
TheinHtay-67 <theinhtay.dev@gmail.com>
2024-06-15 00:12:48 +0630

    feat: Add Promise function for confirmbox

--- THDotNetCore.Html/blog.js
+++ THDotNetCore.Html/blog.js
@@ -10,6 +10,63 @@ let blogId = null;
 // );
 // deleteBlog("219b5970-c917-4aab-b5b8-1a9d6c6340d2");
 getBlogTable();
+// testConfirmMessage2();
+
+function testConfirmMessage() {
+  let confirmMessage = new Promise(function (success, error) {
+    // "Producing Code" (May take some time)
+
+    const result = confirm("Are you sure want to delete?");
+    if (result) {
+      success(); // when successful
+    } else {
+      error(); // when error
+    }
+  });
+
+  // "Consuming Code" (Must wait for a fulfilled Promise)
+  confirmMessage.then(
+    function (value) {
+      /* code if successful */
+      successMessage("Success");
+    },
+    function (error) {
+      /* code if some error */
+      errorMessage("Error");
+    }
+  );
+}
+
+function testConfirmMessage2() {
+  let confirmMessage = new Promise(function (success, error) {
+    // "Producing Code" (May take some time)
+    Swal.fire({
+      title: "Confirm",
+      text: "Are you sure want to delete?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        success(); // when successful
+      } else {
+        error(); // when error
+      }
+    });
+  });
+
+  // "Consuming Code" (Must wait for a fulfilled Promise)
+  confirmMessage.then(
+    function (value) {
+      /* code if successful */
+      successMessage("Success");
+    },
+    function (error) {
+      /* code if some error */
+      errorMessage("Error");
+    }
+  );
+}
 
 function readBlog() {
   let lst = getBlogs();
@@ -82,7 +139,7 @@ function updateBlog(id, title, author, content) {
   successMessage("Update successful!");
 }
 
-function deleteBlog(id) {
+function deleteBlog2(id) {
   let result = confirm("Are you sure want to delete?");
   if (!result) return;
 
@@ -102,13 +159,54 @@ function deleteBlog(id) {
   getBlogTable();
 }
 
-function uuidv4() {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
-    (
-      +c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
-    ).toString(16)
-  );
+function deleteBlog3(id) {
+  Swal.fire({
+    title: "Confirm",
+    text: "Are you sure want to delete?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+  }).then((result) => {
+    if (!result.isConfirmed) return;
+    let lst = getBlogs();
+
+    const items = lst.filter((x) => x.id === id);
+    if (items.length == 0) {
+      console.log("No data found.");
+      return;
+    }
+
+    lst = lst.filter((x) => x.id !== id);
+    const jsonBlog = JSON.stringify(lst);
+    localStorage.setItem(tblBlog, jsonBlog);
+
+    successMessage("Deleting Successful!");
+    getBlogTable();
+  });
+}
+
+function deleteBlog(id) {
+  // "Consuming Code" (Must wait for a fulfilled Promise)
+  confirmMessage("Are you sure want to delete?").then(function (value) {
+    /* code if successful */
+
+    let lst = getBlogs();
+
+    const items = lst.filter((x) => x.id === id);
+    if (items.length == 0) {
+      console.log("No data found.");
+      return;
+    }
+
+    lst = lst.filter((x) => x.id !== id);
+    const jsonBlog = JSON.stringify(lst);
+    localStorage.setItem(tblBlog, jsonBlog);
+
+    successMessage("Deleting Successful!");
+    getBlogTable();
+
+    successMessage("Success");
+  });
 }
 
 function getBlogs() {
@@ -136,24 +234,6 @@ $("#btnSave").click(function () {
 
   getBlogTable();
 });
-
-function successMessage(message) {
-  alert(message);
-  Swal.fire({
-    title: "Success!",
-    text: message,
-    icon: "success",
-  });
-}
-
-function errorMessage(message) {
-  alert(message);
-  Swal.fire({
-    title: "Error!",
-    text: message,
-    icon: "error",
-  });
-}
 
 function clearControls() {
   $("#txtTitle").val("");
