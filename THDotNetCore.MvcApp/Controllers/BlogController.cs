48481edaffb7c2f9d2506a3819a01bcfc5de4893
TheinHtay-67 <theinhtay.dev@gmail.com>
2024-06-22 15:19:08 +0630

    feat: [Add] Blog create in MvcApp

---
+++ THDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using THDotNetCore.MvcApp.Db;
+
+namespace THDotNetCore.MvcApp.Controllers;
+
+public class BlogController : Controller
+{
+    private readonly AppDbContext _db;
+
+    public BlogController(AppDbContext appDbContext)
+    {
+        _db = appDbContext;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var lst = await _db.Blogs.AsNoTracking().ToListAsync();
+        return View(lst);
+    }
+}
