0079bb5dbdb7638fed8c8bd7f2ac30c7766a70c3
TheinHtay-67 <theinhtay.dev@gmail.com>
2024-06-22 13:25:16 +0630

    feat: Add Minimal Api

---
+++ THDotNetCore.MinimalApi/Features/Blog/BlogService.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using THDotNetCore.MinimalApi.Db;
+using THDotNetCore.MinimalApi.Models;
+
+namespace THDotNetCore.MinimalApi.Features.Blog
+{
+    public static class BlogService
+    {
+        public static IEndpointRouteBuilder MapBlogs(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/Blog", async (AppDbContext db) =>
+            {
+                var lst = await db.Blogs.AsNoTracking().ToListAsync();
+                return Results.Ok(lst);
+            });
+
+            app.MapPost("api/Blog", async (AppDbContext db, BlogModel blog) =>
+            {
+                await db.Blogs.AddAsync(blog);
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Saved Successful." : "Saving Failed";
+                return Results.Ok(message);
+            });
+
+            app.MapPut("api/Blog/{id}", async (AppDbContext db, int id, BlogModel blog) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No Data Found");
+                }
+                item.BlogTitle = blog.BlogTitle;
+                item.BlogAuthor = blog.BlogAuthor;
+                item.BlogContent = blog.BlogContent;
+                var result = await db.SaveChangesAsync();
+                string message = result > 0 ? "Updated Successful." : "Updated Failed";
+                return Results.Ok(message);
+            });
+
+            app.MapDelete("api/Blog/{id}", async (AppDbContext db, int id) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No Data Found");
+                }
+                db.Blogs.Remove(item);
+                var result = await db.SaveChangesAsync();
+                string message = result > 0 ? "Delete Successful." : "Delete Failed";
+                return Results.Ok(message);
+            });
+
+            return app;
+        }
+    }
+}
