59183d23af0b8e7d29c734983df65930363f1db6
Thein Htay <theinhtay.dev@gmail.com>
2024-07-14 12:48:12 +0630

    feat: Add new feature ApexChart in MvcChartApp project

---
+++ THDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using THDotNetCore.MvcChartApp.Models;
+
+namespace THDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            PieChartModel model = new PieChartModel();
+            model.Labels = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+            model.Series = new List<int> { 44, 55, 13, 43, 22 };
+
+            return View(model);
+        }
+    }
+}
