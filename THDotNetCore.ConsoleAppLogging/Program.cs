4311b08f36cc6683c5cfcdf6024c1970f36879b0
Thein Htay <theinhtay.dev@gmail.com>
2024-07-14 20:24:01 +0630

    feat: Add Serilog in console app and Add serilog in MvcChartApp

---
+++ THDotNetCore.ConsoleAppLogging/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/THDotNetCore.ConsoleAppLogging.txt", rollingInterval: RollingInterval.Hour)
+            .CreateLogger();
+
+Log.Fatal("Hello, World");
+Log.Error("Error Log");
+
+Console.WriteLine("Hello, World!");
+
+Log.Information("Information Log");
+int a = 10, b = 0;
+try
+{
+    Log.Debug("Dividing {A} by {B}", a, b);
+    Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Something went wrong");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
