6ec9866c51fea010e6df2a143974278734c0c85e
Thein Htay <theinhtay.dev@gmail.com>
2024-07-14 16:01:09 +0630

    feat: Add HighCharts, CanvasJs

---
+++ THDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace THDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
---
+++ THDotNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace THDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+
+        public IActionResult BasicColumnChart()
+        {
+            return View();
+        }
+
+        public IActionResult ResponsiveChart()
+        {
+            return View();
+        }
+    }
+}
